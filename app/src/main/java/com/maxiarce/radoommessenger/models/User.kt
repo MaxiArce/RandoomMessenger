021b898ea26fc57a8251f5b202a4e0a6ac0a6c90
Maxi Arce <maximilianoarce@outlook.com>
2019-03-26 18:56:07 -0300

    Revert "Add EditHabits Screen"
    This reverts commit d6bf88968585371726fef5e75013a12ff8694f36.

--- app/src/main/java/com/maxiarce/radoommessenger/models/User.kt
+++ app/src/main/java/com/maxiarce/radoommessenger/models/User.kt
@@ -4,8 +4,7 @@ import android.os.Parcelable
 import kotlinx.android.parcel.Parcelize
 
 @Parcelize
-class User(val uid: String, val username:String, val profileImageUrl: String, val token: String): Parcelable
-{
+class User(val uid: String, val username:String, val profileImageUrl: String, val token: String): Parcelable{
     constructor() : this("","","","")
 
 }
